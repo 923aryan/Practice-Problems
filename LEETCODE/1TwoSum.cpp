7c5e43785269c35bf483a01d88b7686a9e6dbe78
Aryan <923aryanraj@gmail.com>
2023-06-18 19:09:17 +0530

    final solution

--- LEETCODE/1TwoSum.cpp
+++ LEETCODE/1TwoSum.cpp
@@ -1,10 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
+vector<int> twoSum(vector<int>&nums, int target)
+{
+	vector<int>v;
+	for (int i = 0; i < (int)nums.size(); ++i)
+	{
+		for (int j = i+1; j <= nums.size()-1; ++j)
+		{
+			if(nums[i] + nums[j] == target)
+			{
+				v.push_back(i);
+				v.push_back(j);
+				return v;
+			}
+		}
+	}
+	return v;
+}
 int main()
 {
-	int a;
-	a += a;
-	cin >> a;
-	cout<<"hello"<<" "<<a;
+	vector <int> nums = {3,2,4};
+	int target = 6;
+	vector<int>temp = twoSum(nums, target);
+	for(auto e : temp)
+		cout<<e<<" ";
 	return 0;
 }
